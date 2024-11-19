308b383d5b8be90cbdcd91e72f973710a200e529
TheVinh28 <thevinh2882006@gmail.com>
2024-11-19 21:13:11 +0700

    add hello

---
+++ JavascriptDay6/index.js
@@ -0,0 +1,19 @@
+let countdown;
+let timeLeft = 120;
+function startTimer() {
+clearInterval(countdown);
+countdown = setInterval(() => {
+if (timeLeft > 0) {
+timeLeft--;
+let minutes = Math.floor(timeLeft / 60);
+let seconds = timeLeft % 60;
+document.getElementById("timer").innerHTML = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+} else {
+clearInterval(countdown);
+setTimeout(() => alert("Đã hết giờ"), 3000); 
+}
+}, 1000);
+}
+
+
+
