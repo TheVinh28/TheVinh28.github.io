46b96f768f3ae0928a70cb0c79ac10a6a39dedee
TheVinh28 <thevinh2882006@gmail.com>
2024-12-01 17:01:55 +0700

    add hello

--- JavascriptDay9/index.js
+++ JavascriptDay9/index.js
@@ -1,71 +1,94 @@
-const students = [
-    {
-      id: 1,
-      name: "Nguyễn K Sang",
-      gender: "Nam",
-      mathScore: "7",
-      englishScore: "7",
-      literatureScore: "7",
-    },
-    {
-      id: 2,
-      name: "Nguyễn Huy Hoàng",
-      gender: "Nam",
-      mathScore: "6",
-      englishScore: "7",
-      literatureScore: "9",
-    },
-    {
-      id: 3,
-      name: "Nông Đình Tùng",
-      gender: "Nam",
-      mathScore: "5",
-      englishScore: "6",
-      literatureScore: "6",
-    },
-    {
-      id: 4,
-      name: "Phan Chính Việt",
-      gender: "Nam",
-      mathScore: "6",
-      englishScore: "7",
-      literatureScore: "5",
-    },
-    {
-      id: 5,
-      name: "Phạm Thùy Linh",
-      gender: "Nữ",
-      mathScore: "5",
-      englishScore: "8",
-      literatureScore: "8",
-    },
-  ];
-  
-  (function renderList() {
-    const tbodyDOM = document.getElementById("info-student");
-    tbodyDOM.innerHTML = "";
-    students.forEach((student, index) => {
-      const avgScore = (
-        (parseFloat(student.mathScore) +
-          parseFloat(student.englishScore) +
-          parseFloat(student.literatureScore)) /
-        3
-      ).toFixed(2);
-      const row = document.createElement("tr");
-      row.innerHTML = `
-            <td>${student.id}</td>
-            <td>${student.name}</td>
-            <td>${student.gender}</td>
-            <td>${student.mathScore}</td>
-            <td>${student.englishScore}</td>
-            <td>${student.literatureScore}</td>
-            <td>${avgScore}</td>
-            <td>
-                <button type="button" class="btn btn-primary" style="margin-left: 8px;")">Sửa</button>
-                <button type="button" class="btn btn-danger" style="margin-left: 8px;")">Xoá</button>
-            </td>
-        `;
-      tbodyDOM.appendChild(row);
-    });
-  })();
-  
+//Lấy dữ liệu vào localStorage
+let initData = localStorage.getItem("students")
+  ? JSON.parse(localStorage.getItem("students"))
+  : [];
+
+//Hàm trả về viết bằng arrow function
+const averageScore = (mathScore, englishScore, literatureScore) => {
+  return (
+    (parseFloat(mathScore) +
+      parseFloat(englishScore) +
+      parseFloat(literatureScore)) /
+    3
+  ).toFixed(2);
+};
+
+//Hàm trả về viết bằng function
+// function averageScore(mathScore, englishScore, literatureScore) {
+//   return (
+//     (parseFloat(mathScore) +
+//       parseFloat(englishScore) +
+//       parseFloat(literatureScore)) /
+//     3
+//   );
+// };
+
+function renderStudents(data) {
+  const studentTable = document.getElementById("info-student");
+  studentTable.innerHTML = "";
+  data.forEach((student, index) => {
+    const row = `
+      <tr>
+        <td>${index + 1}</td>
+        <td>${student.name}</td>
+        <td>${student.gender}</td>
+        <td>${student.mathScore}</td>
+        <td>${student.englishScore}</td>
+        <td>${student.literatureScore}</td>
+        <td>${averageScore(
+          student.mathScore,
+          student.englishScore,
+          student.literatureScore
+        )}</td>
+        <td>
+        <button class="btn btn-primary btn-sm")">Sửa</button>
+          <button class="btn btn-danger btn-sm" onclick="deleteStudent(${index})">Xóa</button>
+        </td>
+      </tr>
+    `;
+    studentTable.insertAdjacentHTML("beforeend", row);
+  });
+}
+
+renderStudents(initData);
+
+//Hàm tạo học sinh mới
+document.getElementById("studentForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const gender = document.querySelector('input[name="gender"]:checked')?.value;
+  const mathScore = parseFloat(document.getElementById("mathScore").value);
+  const englishScore = parseFloat(
+    document.getElementById("englishScore").value
+  );
+  const literatureScore = parseFloat(
+    document.getElementById("literatureScore").value
+  );
+
+  if (
+    !name ||
+    !gender ||
+    isNaN(mathScore) ||
+    isNaN(englishScore) ||
+    isNaN(literatureScore)
+  ) {
+    alert("Vui lòng nhập đầy đủ và chính xác thông tin!");
+    return;
+  }
+  const newStudent = { name, gender, mathScore, englishScore, literatureScore };
+  initData.push(newStudent);
+  //Lưu dữ liệu vào localStorage
+  localStorage.setItem("students", JSON.stringify(initData));
+  renderStudents(initData);
+  e.target.reset();
+});
+
+//Hàm xóa học sinh
+function deleteStudent(index){
+  initData.splice(index,1);
+  localStorage.setItem("students", JSON.stringify(initData));
+  renderStudents(initData);
+};
+//slice không làm ảnh hưởng tới mảng gốc
+//splice làm thay đổi cấu trúc của mảng gốc
